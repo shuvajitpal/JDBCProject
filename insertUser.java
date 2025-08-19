e11b33ea824ff0dc41643409ddb13358e5c8a476
shuvajitpal <62866829+shuvajitpal@users.noreply.github.com>
2025-08-19 12:31:15 +0530

    Update insertUser.java

--- insertUser.java
+++ insertUser.java
@@ -2,10 +2,14 @@ package com.Broskieshub.JDBCProject;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 public class insertUser {
    public static void main(String[] args) {
       Connection con = null;
       PreparedStatement ps = null;
+      Statement st = null;
+      ResultSet rs = null;
       try {
          String url = "jdbc:mysql://localhost:3306/testdb";
          String user = "root";
@@ -20,6 +24,7 @@ public class insertUser {
 
          ps.setString(1, "Shuvajit Pal");
          ps.setString(2, "shuvajitpal103@gmail.com");
+         ps.executeUpdate();
 
          ps.setString(1, "Debasish Behera");
          ps.setString(2, "debasish@tech69.com");
@@ -31,6 +36,18 @@ public class insertUser {
 
          int rowsInserted = ps.executeUpdate();
          if (rowsInserted > 0) System.out.println("A new user was inserted successfully!");
+
+         st = con.createStatement();
+         rs = st.executeQuery("SELECT * FROM user");
+
+         System.out.println("\nUser Table Data:");
+         while (rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            String email = rs.getString("email");
+
+            System.out.println(id + " | " + name + " | " + email);
+         }
       } catch (Exception e) {
          e.printStackTrace();
       } finally {
